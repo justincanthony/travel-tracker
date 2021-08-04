915e0554cbe4975c8a255344ab6bb3f6b9d47343
Justin Anthony <justincanthony@yahoo.com>
2021-08-04 02:16:21 -0600

    Set up structure and imports

---
+++ src/domUpdates.js
@@ -0,0 +1,28 @@
+// import /*Classes*/ from '/*filepath*/';
+// import { /*global variables*/ } from './scripts.js';
+// import [Node Packages] from 'filename';
+
+
+
+
+const displayAllData = () => {
+  displayTravelerData();
+  displayDestinationData();
+  displayTripData();
+};
+
+
+const displayTravelerData = () => {
+
+}
+
+const displayDestinationData = () => {
+
+}
+
+const displayTripData = () => {
+
+}
+
+
+export default displayAllData;
