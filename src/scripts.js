48cdf088407a83e979b8904814308436956a6832
Justin Anthony <justincanthony@yahoo.com>
2021-08-04 02:15:54 -0600

    Set up imports and orgranize structure

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,45 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+// ******************************************************import styling pages here
 import './css/base.scss';
+import './css/styles.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// **********************************************************importing images here
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+// *********************************************************importing classes here
+import Class from './file';
+import Class from './file';
+import Class from './file';
+import Class from './file';
+import Class from './file';
+
+// ************************************************************importing funcitons
+import displayAllData from './updateDOM';
+import getAllData from './apiCalls';
+
+// *****************************************************exporting global variables
+
+// export { [global variables declared below] };
+
+
+// ************************************************declaration of global variables
+
+// let global [variable declared above];
+
+
+// *************************************************resolving promise from api call
+// All data will be returned to this function in an array
+// will need to access with bracket notation
+// possible to use iterator here to get the user foirm login
+// maybe the agency holds all data and things are instantiated from that as needed
+
+getAllData()
+  .then((data) => {
+    traveler = new Traveler();
+    travelAgency = new Agency();
+    destination = new Destination();
+    trip = new Trip();
+    agent = new Agent();
+    );
+  })
+  .then(displayAllData);
