d9a3b027c1555795c4fd210e1d3e5a7e227eb6ae
Justin Anthony <justincanthony@gmail.com>
2021-08-04 12:06:28 -0600

    Change name to agent

---
+++ src/Agent.js
@@ -0,0 +1,10 @@
+// class Agent {
+//   constructor(travelersData, tripsData, destinationsData, id) {
+//     this.travelersData = travelersData;
+//     this.tripsData = tripsData;
+//     this.destinationData = destinationsData;
+//     this.singleUser =
+
+//   }
+// }
+// export default Agent;
