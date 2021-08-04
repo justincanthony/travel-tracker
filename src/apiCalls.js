1f2bf3a3711218e7e55fc06bdf84b21caf3bbb4d
Justin Anthony <justincanthony@gmail.com>
2021-08-04 12:07:17 -0600

    Comment out code causing display errors

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ const getData = (suffix) => {
   const baseURL = 'http://localhost:3001/api/v1/';
   return fetch(`${baseURL}${suffix}`).then((response) => response.json());
 };
-  
+
 const getAllData = () => {
   return Promise.all([
     getData('travelers'),
@@ -12,5 +12,37 @@ const getAllData = () => {
   ]);
 };
 
+// ****************************************Get User & Trip
+// Input for UsernameID
+// Input For password
+// if password === travel, then fire Get User and Trip Data
+
 export default getAllData;
-  
+
+// Error Handling
+
+// export const displayError = (response) => {
+//   if (!response.ok) {
+//     console.log('Something went wrong, try again!')
+//   } else {
+//     console.log(response.json())
+//     return response.json()
+//   }
+// }
+
+// export const getTraveler = (path) => {
+//   return fetch(`${endPoint}${path}`)
+//     .then(response => displayError(response))
+//     .then(data => data)
+//     .catch(error => console.error(error))
+// }
+
+// /////////// eror handling /////////
+
+// export const displayError = (response) => {
+//   if (!response.ok) {
+//     console.log('Something went wrong, try again!')
+//   } else {
+//     console.log(response.json())
+//   }
+// }
