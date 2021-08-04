3e181432d84015aef124adbbc923659ff97e7e11
Justin Anthony <justincanthony@yahoo.com>
2021-08-04 01:28:52 -0600

    Format style

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,16 @@
 const getData = (suffix) => {
-    const baseURL = 'http://localhost:3001/api/v1/';
-    return fetch(`${baseURL}${suffix}`).then((response) => response.json());
-  };
+  const baseURL = 'http://localhost:3001/api/v1/';
+  return fetch(`${baseURL}${suffix}`).then((response) => response.json());
+};
   
-  const getAllData = () => {
-    return Promise.all([
-      getData('travelers'),
-      getData('travelers/1'),
-      getData('trips'),
-      getData('destinations'),
-    ]);
-  };
-  export default getAllData;
+const getAllData = () => {
+  return Promise.all([
+    getData('travelers'),
+    getData('travelers/1'),
+    getData('trips'),
+    getData('destinations'),
+  ]);
+};
+
+export default getAllData;
   
