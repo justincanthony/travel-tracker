c1adccb5eeb4b5251b1970dbea01f129f5ad9038
Justin Anthony <justincanthony@gmail.com>
2021-08-08 21:26:17 -0600

    Update methods for the class

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,8 +1,34 @@
+let dayjs = require('dayjs');
 class Traveler {
   constructor(travelerData) {
     this.UserID = travelerData.id;
     this.name = travelerData.name;
     this.travelerType = travelerData.travelerType;
+    this.trips = [];
+  }
+
+  getPendingTrips() {
+    return this.trips.filter((trip) => trip.status === 'pending');
+  }
+
+  getApprovedTrips() {
+    return this.trips.filter((trip) => trip.status === 'approved');
+  }
+
+  getFutureTrips(date) {
+    return this.trips.filter((trip) => {
+      if (dayjs(date).isBefore(dayjs(trip.date))) {
+        return trip;
+      }
+    });
+  }
+
+  getPastTrips(date) {
+    return this.trips.filter((trip) => {
+      if (dayjs(date).isAfter(dayjs(trip.date))) {
+        return trip;
+      }
+    });
   }
 }
 export default Traveler;
