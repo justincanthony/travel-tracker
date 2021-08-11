6f69313bbde0f1df948265243575fd11d85c274d
Justin Anthony <justincanthony@gmail.com>
2021-08-10 19:12:32 -0600

    Attempt to debug post fetch

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,12 +1,43 @@
 // import /*Classes*/ from '/*filepath*/';
 import Destination from './Destination.js';
 import { traveler, agency } from './scripts.js';
+import requestTrip from './apiCalls.js';
 let dayjs = require('dayjs');
 
 let todaysDate = dayjs();
-
+const pendingTrips = document.getElementById('pendingTrips');
+const submit = document.getElementById('submitButton');
+const durationInput = document.getElementById('duration');
+const travelersInput = document.getElementById('travelers');
 const startDateInput = document.getElementById('start');
 const destinationDropDown = document.getElementById('optionSelect');
+let tripObject;
+// const option = destinationDropDown.options[destinationDropDown.selectedIndex];
+// const value =
+//   destinationDropDown.options[destinationDropDown.selectedIndex].value;
+console.log(destinationDropDown.value)
+
+// event.preventDefault();
+  tripObject = {
+    id: 2361,
+    userID: 12,
+    destinationID: 10,
+    travelers: 1,
+    date: '2022/01/28',
+    duration: 18,
+    status: 'approved',
+    suggestedActivities: [],
+  };
+
+
+submit.addEventListener('click', (event, tripObject) => {
+  // 
+  requestTrip(event, tripObject);
+});
+
+export const addTripToPage = (trip) => {
+  pendingTrips.innerHTML += `<p>${trip.name}</p>`;
+};
 
 const displayAllData = () => {
   displayTravelerData();
@@ -27,27 +58,24 @@ const displayTravelerData = () => {
 const displayDestinationData = () => {
   agency.destinations.forEach((destination) => {
     document.getElementById('destinations').innerHTML += `<article class="card">
-      <div class="card-header">
-        <img src=${destination.image} class="destination-picture">
+    <div class="card-header">
+      <img src=${destination.image} alt=${
+      destination.alt
+    } class="destination-picture">
+    </div>
+    <div class="card-main">
+      <div class="card-div">
+        <h3>${destination.destination}</h3>
+        <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
       </div>
-      <div class="card-main">
-        <div class="card-div">
-          <h3>${destination.destination}</h3>
-          <h5>Date 02/23/16 10:22am</h5>
-        </div>
-        <div class="card-div-grey">
-          <h5></h5>
-        </div>
-        <div class="card-div">
-          <h5></h5>
-          <h3></h3>
-        </div>
-        <div class="card-div">
-          <h5>Cost</h5>
-          <h3 id="blue"></h3>
-        </div> 
+      <div class="card-div-grey">
+      <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
+        <h5>Flight Cost Per Person $${
+          destination.estimatedFlightCostPerPerson
+        }</h5>
       </div>
-    </article>`;
+    </div>
+  </article>`;
   });
 };
 
@@ -67,24 +95,24 @@ const displayFutureTripData = () => {
 
   approvedDestinations.forEach((destination) => {
     document.getElementById('future-trips').innerHTML += `<article class="card">
-      <div class="card-header">
-        <img src=${destination.image} alt=${
+    <div class="card-header">
+      <img src=${destination.image} alt=${
       destination.alt
     } class="destination-picture">
+    </div>
+    <div class="card-main">
+      <div class="card-div">
+        <h3>${destination.destination}</h3>
+        <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
       </div>
-      <div class="card-main">
-        <div class="card-div">
-          <h3>${destination.destination}</h3>
-          <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
-        </div>
-        <div class="card-div-grey">
-        <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
-          <h5>Flight Cost Per Person $${
-            destination.estimatedFlightCostPerPerson
-          }</h5>
-        </div>
+      <div class="card-div-grey">
+      <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
+        <h5>Flight Cost Per Person $${
+          destination.estimatedFlightCostPerPerson
+        }</h5>
       </div>
-    </article>`;
+    </div>
+  </article>`;
   });
 };
 
@@ -106,24 +134,24 @@ const displayPendingTrips = () => {
     document.getElementById(
       'pending-trips'
     ).innerHTML += `<article class="card">
-      <div class="card-header">
-        <img src=${destination.image} alt=${
+    <div class="card-header">
+      <img src=${destination.image} alt=${
       destination.alt
     } class="destination-picture">
+    </div>
+    <div class="card-main">
+      <div class="card-div">
+        <h3>${destination.destination}</h3>
+        <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
       </div>
-      <div class="card-main">
-        <div class="card-div">
-          <h3>${destination.destination}</h3>
-          <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
-        </div>
-        <div class="card-div-grey">
-        <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
-          <h5>Flight Cost Per Person $${
-            destination.estimatedFlightCostPerPerson
-          }</h5>
-        </div>
+      <div class="card-div-grey">
+      <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
+        <h5>Flight Cost Per Person $${
+          destination.estimatedFlightCostPerPerson
+        }</h5>
       </div>
-    </article>`;
+    </div>
+  </article>`;
   });
 };
 
@@ -145,24 +173,24 @@ const displayPastTrips = () => {
     document.getElementById(
       'pending-trips'
     ).innerHTML += `<article class="card">
-      <div class="card-header">
-        <img src=${destination.image} alt=${
+    <div class="card-header">
+      <img src=${destination.image} alt=${
       destination.alt
     } class="destination-picture">
+    </div>
+    <div class="card-main">
+      <div class="card-div">
+        <h3>${destination.destination}</h3>
+        <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
       </div>
-      <div class="card-main">
-        <div class="card-div">
-          <h3>${destination.destination}</h3>
-          <h5>${dayjs(destination.date).format('M/DD/YY')}</h5>
-        </div>
-        <div class="card-div-grey">
-        <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
-          <h5>Flight Cost Per Person $${
-            destination.estimatedFlightCostPerPerson
-          }</h5>
-        </div>
+      <div class="card-div-grey">
+      <h5>Lodging Cost Per Day $${destination.estimatedLodgingCostPerDay}</h5>
+        <h5>Flight Cost Per Person $${
+          destination.estimatedFlightCostPerPerson
+        }</h5>
       </div>
-    </article>`;
+    </div>
+  </article>`;
   });
 };
 
@@ -207,5 +235,13 @@ const populateDropDownMenu = () => {
     destinationDropDown.innerHTML += `<option value="${destination.id}">${destination.destination}</option>`;
   });
 };
+
+const show = (element) => {
+  element.classList.remove('hidden');
+}
+
+const hide = (element) => {
+  element.classList.add('hidden');
+}
 
 export default displayAllData;
